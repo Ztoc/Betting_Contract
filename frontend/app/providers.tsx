f3c6b701c3939f0f3a5acc523dd889b38c3dcae6
Ryder <bhu0444@gmail.com>
2024-06-13 07:25:50 -0400

    feat: add wallet connect

---
+++ frontend/app/providers.tsx
@@ -0,0 +1,20 @@
+"use client";
+
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { WagmiProvider } from "wagmi";
+import { config } from "@/config/Config";
+import { RainbowKitProvider } from "@rainbow-me/rainbowkit";
+
+const queryClient = new QueryClient();
+
+export const Providers = ({ children }: { children: React.ReactNode }) => {
+  return (
+    <>
+      <WagmiProvider config={config}>
+        <QueryClientProvider client={queryClient}>
+          <RainbowKitProvider>{children}</RainbowKitProvider>
+        </QueryClientProvider>
+      </WagmiProvider>
+    </>
+  );
+};
